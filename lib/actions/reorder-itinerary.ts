9fd317835fa5be863ebd315c55a744db7056f31c
Legaz <worldoflegaz@icloud.com>
2025-07-12 16:26:28 +0900

    able to change value in database

--- lib/actions/reorder-itinerary.ts
+++ lib/actions/reorder-itinerary.ts
@@ -1,6 +1,20 @@
 "use server";
 
+import { auth } from "@/auth";
+import { prisma } from "../prisma";
 
-export async function reorderItinerary(tripId: string, newOrder: string[]){
-    
+export async function reorderItinerary(tripId: string, newOrder: string[]) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("You must be signed in to add a location.");
+  }
+
+  await prisma.$transaction(
+    newOrder.map((locationId: string, key: number) =>
+      prisma.location.update({
+        where: { id: locationId },
+        data: { order: key },
+      })
+    )
+  );
 }
